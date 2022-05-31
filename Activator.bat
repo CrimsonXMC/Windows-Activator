a96b50498b09f4bb89ca3dfe53417f19a97a12ce
CrimsonXMC <58105881+CrimsonXMC@users.noreply.github.com>
2022-05-31 03:49:30 +0100

    v1.2.0
    Made the UI look significantly better with some nice ascii art.

--- Activator.bat
+++ Activator.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBdRWg2+GGStCLkT6ezo08uEtkQLVfYwasHewrHu
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
 :: BatchGotAdmin
@@ -26,8 +59,15 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 
 @ECHO OFF
+color a
 CLS
-ECHO Windows Activator by IcanLevitate. Please choose your windows Edition.
+:: this ascii art is really hacky... if someone else wants to remake it to not be hacky, feel free.
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/CrimsonXMC/Windows-Activator/main/ascii.txt', '%tmp%\ascii.txt')"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/CrimsonXMC/Windows-Activator/main/ascii.txt -OutFile %tmp%\ascii.txt"
+TYPE %TMP%\ascii.txt
+DEL %TMP%\ascii.txt
+:: i mean at least i delete the text file after its done with 
+ECHO Pick which version of Windows you want to activate.
 ECHO 1.Windows 10/11 Pro
 ECHO 2.Windows 10/11 Pro N
 ECHO 3.Windows 10/11 Pro For Workstations
